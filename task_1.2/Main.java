a9ffd69e2cdfb1a1611b076c57af1aca3f76bfee
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-18 17:30:29 +0300

    Create Main.java

---
+++ task_1.2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.print("два"); // лишняя строка
+        System.out.print(b);
+        System.out.print(" плюс ");
+        System.out.print(" минус "); // лишняя строка
+        System.out.print(a);
+        System.out.print("три"); // лишняя строка
+        System.out.print(" равно ");
+        System.out.print(" будет "); // лишняя строка
+        System.out.print("пять");
+        System.out.print(a + b); // лишняя строка
+    }
+}
