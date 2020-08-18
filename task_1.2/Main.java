1e807ff73655ef8ed3e8c5e5538e825972a9ecbc
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-18 17:42:29 +0300

    Update Main.java

--- task_1.2/Main.java
+++ task_1.2/Main.java
@@ -3,15 +3,15 @@ public class Main {
         int a = 3;
         int b = 2;
 
-        System.out.print("два"); // лишняя строка
+//        System.out.print("два"); // лишняя строка
         System.out.print(b);
         System.out.print(" плюс ");
-        System.out.print(" минус "); // лишняя строка
+//        System.out.print(" минус "); 
         System.out.print(a);
-        System.out.print("три"); // лишняя строка
+//        System.out.print("три"); 
         System.out.print(" равно ");
-        System.out.print(" будет "); // лишняя строка
-        System.out.print("пять"); // лишняя строка
+//        System.out.print(" будет "); 
+//        System.out.print("пять"); 
         System.out.print(a + b); 
     }
 }
