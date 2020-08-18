b84df2dc4e7b7bbeb0a8b1780cd040b37e4d9a06
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-18 17:39:18 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int a;
+    a = 19;
+    int b;
+    b = 90;
+    String s = "Перемен";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
