2dbf5b9b8664018628846868ba3705aee49698fe
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-18 16:12:30 +0300

    Update Main.java

--- task_1.1/Main.java
+++ task_1.1/Main.java
@@ -1,1 +1,15 @@
 
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+    String a = "Марианна";
+        int age;
+        age = 18;
+    String b = "Москва";
+    System.out.println(a);
+    System.out.println("всегда "+age);
+    System.out.println(b);
+    }
+}
