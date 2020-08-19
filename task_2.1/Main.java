26ebcf234def89827fa75a2401e1844da236b127
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-19 17:33:24 +0300

    Create Main.java

---
+++ task_2.1/Main.java
@@ -0,0 +1,28 @@
+/* Написать функцию, которая вычисляет минимум из трёх чисел.
+Требования:
+•	Программа должна выводить текст на экран.
+•	Метод min не должен выводить текст на экран.
+•	Метод main должен вызвать метод min четыре раза.
+•	Метод main должен выводить на экран результат работы метода min. Каждый раз с новой строки.
+•	Метод min должен возвращать минимальное значение из чисел a, b и с.
+*/
+
+public class Main {
+    public static int min(int a, int b, int c) {
+        //напишите тут ваш код
+        if (a>b) return b;
+        else if (b>a) return a;
+        else if (c>a) return a;
+        else if (c>b) return b;
+        else if (b>c) return c;
+        else if (a>c) return c;
+        else if (c>b) return b;
+        else return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
