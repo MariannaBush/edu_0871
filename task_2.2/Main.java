44afdc036132967a7f4c71b1a559db8d7e4964d6
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-08-21 17:29:58 +0300

    Create Main.java

---
+++ task_2.2/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static int min(int a, int b, int c, int d) {
+        return (min(min(a, b),min(c,d)));
+    }
+
+    public static int min(int a, int b) {
+        if (a>b) return b;
+        else return a;
+      //   else if (b>a) return a; - лишняя строка
+      //   else return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+}
