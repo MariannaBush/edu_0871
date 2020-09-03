82e65cfb68dd581068978fe91795d4a5070d0d2c
MariannaBush <69797709+MariannaBush@users.noreply.github.com>
2020-09-03 15:20:44 +0300

    Update Client.java

--- Chat/Client.java
+++ Chat/Client.java
@@ -1,6 +1,8 @@
 package client;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,17 +12,16 @@ public class Client {
             Socket socket = new Socket("localhost",8189);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
             Scanner scanner = new Scanner(System.in);
-
-          //  System.out.println("Введите ник: " +nickname);
-//            out.write(Integer.parseInt(scanner.nextLine()));
-//            Resender resend = new Resender();
-//            resend.start();
-
             Thread responseThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    while (true) {
+                    System.out.println("Введите ник и нажмите enter: ");
+                    String nickname = "";
+                //    System.out.println(nickname+" зашел в чат!");
+                    while (true){
+
                         String response = null;
                         try {
                             response = in.readUTF();
@@ -32,41 +33,12 @@ public class Client {
                 }
             });
             responseThread.start();
-            while (true) {
+            while (true){
                 String str = scanner.nextLine();
                 out.writeUTF(str);
-
-            //    String response = in.readUTF(); //ждет ответ от сервера
-            //    System.out.println(in.readUTF());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static void getNick(Socket clientSocket) throws IOException {
-        String nickname = null;
-        Writer output = new OutputStreamWriter(new BufferedOutputStream(clientSocket.getOutputStream()));
-        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        output.write("Введите ник:\n +\r");
-        nickname = input.readLine();
-    }
-//    private class Resender extends Thread {
-//        private boolean stoped;
-//
-//        public void setStop() {
-//            stoped = true;
-//        }
-//        @Override
-//        public void run() {
-//            try {
-//                while (!stoped) {
-//                    String str = in.readLine();
-//                    System.out.println(str);
-//                }
-//            } catch (IOException e) {
-//                System.err.println("Ошибка при получении сообщения.");
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
